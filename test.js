131ed9788cef0b37bc9b791b2d07845e2272e8b1
Maksym Kachalai <maks.kachalay@gmail.com>
2023-02-24 04:09:15 +0200

    create react template

---
+++ test.js
@@ -0,0 +1,19 @@
+import { ThemeOptions } from "@mui/material/styles";
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#2b2d42",
+    },
+    secondary: {
+      main: "#ef233c",
+    },
+    error: {
+      main: "#d32f2f",
+    },
+    success: {
+      main: "#8d99ae",
+    },
+  },
+};
