4370a8902fb908be5a7eb738a9629aa464b5d8c5
Maksym Kachalai <maks.kachalay@gmail.com>
2023-03-20 00:36:21 +0200

    fix all

--- src/form.js
+++ src/form.js
@@ -32,3 +32,45 @@ form.addEventListener('submit', event => {
     input.value = '';
   }
 });
+
+const applicationForm = document.getElementById('application__form');
+const applicationInputName = document.getElementById('application__input-name');
+const applicationInputPhone = document.getElementById(
+  'application__input-phone'
+);
+
+applicationForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const { phone, name } = event.target;
+  const { result } = isValidPhone(phone.value);
+  if (name.value === '') {
+    applicationInputName.setCustomValidity('error');
+    Notify.failure('Заповніть всі поля');
+    setTimeout(() => {
+      applicationInputName.setCustomValidity('');
+    }, 1000);
+    return;
+  }
+  if (!result) {
+    applicationInputPhone.setCustomValidity('error');
+    Notify.failure('Неправильний формат номеру');
+    setTimeout(() => {
+      applicationInputPhone.setCustomValidity('');
+    }, 1000);
+  }
+  if (result) {
+    Report.success(
+      'Заявку прийнято',
+      "Ми зв'яжемося з Вами протягом 5 хвилин",
+      'Чудово!',
+      {
+        fontFamily: 'Montserrat',
+        titleFontSize: '20px',
+        messageFontSize: '16px',
+        buttonFontSize: '16px',
+      }
+    );
+    applicationInputName.value = '';
+    applicationInputPhone.value = '';
+  }
+});
