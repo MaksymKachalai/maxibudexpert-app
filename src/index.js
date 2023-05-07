a4c163608cfd251580ce2967dad3613695f0f6cf
Maksym Kachalai <maks.kachalay@gmail.com>
2023-05-07 22:43:42 +0300

    delete basename router

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/maxibudexpert-app">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
