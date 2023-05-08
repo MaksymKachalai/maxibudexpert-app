f2070260868ee87a649008d977b7dc604dc5ea54
Maksym Kachalai <maks.kachalay@gmail.com>
2023-05-08 21:10:02 +0300

    add server

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const TelegramBot = require('node-telegram-bot-api');
+const { body, validationResult } = require('express-validator');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+console.log(path.join(__dirname, '..', 'client', 'build'));
+
+const bot = new TelegramBot(process.env.token, { polling: true });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build'));
+});
+
+app.post('/user', body('phone').isMobilePhone('uk-UA'), body('name').trim(), (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400);
+  }
+  const { name, phone } = req.body;
+  const telegramMessage = `Ім'я: ${name} \nНомер телефону: ${phone}`;
+  res.json({
+    status: 'ok',
+  });
+  bot.sendMessage(process.env.id, telegramMessage);
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
